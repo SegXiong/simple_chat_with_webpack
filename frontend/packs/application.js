b36f32e1dac485f7a3ef5208c809097969bdffbb
segxiong <segxiong0817@gmail.com>
2018-08-14 14:52:28 +0800

    part 1

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,3 +1,3 @@
-import "./application.css";
-
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works! hahaha");
+import "init";
+import "components/page/page";
+import "components/auth-form/auth-form";
